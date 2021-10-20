8696f58dba58a9e4c8367bc87db8787c7962ab24
Burdiugh <burdiugh.bk@gmail.com>
2021-10-20 18:49:43 +0300

    Add project files.

---
+++ OrderWinForm/Form1.Designer.cs
@@ -0,0 +1,420 @@
+﻿
+namespace OrderWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FriedEggsRB = new System.Windows.Forms.RadioButton();
+            this.PorridgeRB = new System.Windows.Forms.RadioButton();
+            this.MeatRB = new System.Windows.Forms.RadioButton();
+            this.PotatoRB = new System.Windows.Forms.RadioButton();
+            this.vegetablesRB = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cocacolaRB = new System.Windows.Forms.RadioButton();
+            this.pepsiRB = new System.Windows.Forms.RadioButton();
+            this.juiceRB = new System.Windows.Forms.RadioButton();
+            this.teaRB = new System.Windows.Forms.RadioButton();
+            this.beerRB = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chocCakeRB = new System.Windows.Forms.RadioButton();
+            this.iceCream = new System.Windows.Forms.RadioButton();
+            this.donatRB = new System.Windows.Forms.RadioButton();
+            this.cheeseRB = new System.Windows.Forms.RadioButton();
+            this.zefirRB = new System.Windows.Forms.RadioButton();
+            this.desert = new System.Windows.Forms.GroupBox();
+            this.drinks = new System.Windows.Forms.GroupBox();
+            this.mainCourses = new System.Windows.Forms.GroupBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.showBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.desert.SuspendLayout();
+            this.drinks.SuspendLayout();
+            this.mainCourses.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Order App";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(23, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(302, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Choose dishes to order:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(25, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Main course: ";
+            // 
+            // FriedEggsRB
+            // 
+            this.FriedEggsRB.AutoSize = true;
+            this.FriedEggsRB.Location = new System.Drawing.Point(6, 19);
+            this.FriedEggsRB.Name = "FriedEggsRB";
+            this.FriedEggsRB.Size = new System.Drawing.Size(89, 17);
+            this.FriedEggsRB.TabIndex = 9;
+            this.FriedEggsRB.TabStop = true;
+            this.FriedEggsRB.Text = "Fried eggs 2$";
+            this.FriedEggsRB.UseVisualStyleBackColor = true;
+            // 
+            // PorridgeRB
+            // 
+            this.PorridgeRB.AutoSize = true;
+            this.PorridgeRB.Location = new System.Drawing.Point(6, 42);
+            this.PorridgeRB.Name = "PorridgeRB";
+            this.PorridgeRB.Size = new System.Drawing.Size(79, 17);
+            this.PorridgeRB.TabIndex = 10;
+            this.PorridgeRB.TabStop = true;
+            this.PorridgeRB.Text = "Porridge 3$";
+            this.PorridgeRB.UseVisualStyleBackColor = true;
+            // 
+            // MeatRB
+            // 
+            this.MeatRB.AutoSize = true;
+            this.MeatRB.Location = new System.Drawing.Point(6, 65);
+            this.MeatRB.Name = "MeatRB";
+            this.MeatRB.Size = new System.Drawing.Size(145, 17);
+            this.MeatRB.TabIndex = 11;
+            this.MeatRB.TabStop = true;
+            this.MeatRB.Text = "Meat with sendwiches 8$";
+            this.MeatRB.UseVisualStyleBackColor = true;
+            // 
+            // PotatoRB
+            // 
+            this.PotatoRB.AutoSize = true;
+            this.PotatoRB.Location = new System.Drawing.Point(6, 88);
+            this.PotatoRB.Name = "PotatoRB";
+            this.PotatoRB.Size = new System.Drawing.Size(96, 17);
+            this.PotatoRB.TabIndex = 12;
+            this.PotatoRB.TabStop = true;
+            this.PotatoRB.Text = "Fried potato 2$";
+            this.PotatoRB.UseVisualStyleBackColor = true;
+            // 
+            // vegetablesRB
+            // 
+            this.vegetablesRB.AutoSize = true;
+            this.vegetablesRB.Location = new System.Drawing.Point(6, 111);
+            this.vegetablesRB.Name = "vegetablesRB";
+            this.vegetablesRB.Size = new System.Drawing.Size(124, 17);
+            this.vegetablesRB.TabIndex = 13;
+            this.vegetablesRB.TabStop = true;
+            this.vegetablesRB.Text = "Grilled vegetables 6$";
+            this.vegetablesRB.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(192, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Drinks: ";
+            // 
+            // cocacolaRB
+            // 
+            this.cocacolaRB.AutoSize = true;
+            this.cocacolaRB.Location = new System.Drawing.Point(6, 19);
+            this.cocacolaRB.Name = "cocacolaRB";
+            this.cocacolaRB.Size = new System.Drawing.Size(95, 17);
+            this.cocacolaRB.TabIndex = 15;
+            this.cocacolaRB.TabStop = true;
+            this.cocacolaRB.Text = "Coca Cola 50c";
+            this.cocacolaRB.UseVisualStyleBackColor = true;
+            // 
+            // pepsiRB
+            // 
+            this.pepsiRB.AutoSize = true;
+            this.pepsiRB.Location = new System.Drawing.Point(6, 42);
+            this.pepsiRB.Name = "pepsiRB";
+            this.pepsiRB.Size = new System.Drawing.Size(72, 17);
+            this.pepsiRB.TabIndex = 16;
+            this.pepsiRB.TabStop = true;
+            this.pepsiRB.Text = "Pepsi 50c";
+            this.pepsiRB.UseVisualStyleBackColor = true;
+            // 
+            // juiceRB
+            // 
+            this.juiceRB.AutoSize = true;
+            this.juiceRB.Location = new System.Drawing.Point(6, 65);
+            this.juiceRB.Name = "juiceRB";
+            this.juiceRB.Size = new System.Drawing.Size(85, 17);
+            this.juiceRB.TabIndex = 17;
+            this.juiceRB.TabStop = true;
+            this.juiceRB.Text = "Fruit juice 1$";
+            this.juiceRB.UseVisualStyleBackColor = true;
+            // 
+            // teaRB
+            // 
+            this.teaRB.AutoSize = true;
+            this.teaRB.Location = new System.Drawing.Point(6, 88);
+            this.teaRB.Name = "teaRB";
+            this.teaRB.Size = new System.Drawing.Size(71, 17);
+            this.teaRB.TabIndex = 18;
+            this.teaRB.TabStop = true;
+            this.teaRB.Text = " Tea 1.5$";
+            this.teaRB.UseVisualStyleBackColor = true;
+            // 
+            // beerRB
+            // 
+            this.beerRB.AutoSize = true;
+            this.beerRB.Location = new System.Drawing.Point(6, 111);
+            this.beerRB.Name = "beerRB";
+            this.beerRB.Size = new System.Drawing.Size(62, 17);
+            this.beerRB.TabIndex = 19;
+            this.beerRB.TabStop = true;
+            this.beerRB.Text = "Beer 3$";
+            this.beerRB.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(313, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Deserts: ";
+            // 
+            // chocCakeRB
+            // 
+            this.chocCakeRB.AutoSize = true;
+            this.chocCakeRB.Location = new System.Drawing.Point(6, 19);
+            this.chocCakeRB.Name = "chocCakeRB";
+            this.chocCakeRB.Size = new System.Drawing.Size(116, 17);
+            this.chocCakeRB.TabIndex = 21;
+            this.chocCakeRB.TabStop = true;
+            this.chocCakeRB.Text = "Chocolate Cake 3$";
+            this.chocCakeRB.UseVisualStyleBackColor = true;
+            // 
+            // iceCream
+            // 
+            this.iceCream.AutoSize = true;
+            this.iceCream.Location = new System.Drawing.Point(6, 42);
+            this.iceCream.Name = "iceCream";
+            this.iceCream.Size = new System.Drawing.Size(88, 17);
+            this.iceCream.TabIndex = 22;
+            this.iceCream.TabStop = true;
+            this.iceCream.Text = "Ice Cream 2$";
+            this.iceCream.UseVisualStyleBackColor = true;
+            // 
+            // donatRB
+            // 
+            this.donatRB.AutoSize = true;
+            this.donatRB.Location = new System.Drawing.Point(6, 65);
+            this.donatRB.Name = "donatRB";
+            this.donatRB.Size = new System.Drawing.Size(69, 17);
+            this.donatRB.TabIndex = 23;
+            this.donatRB.TabStop = true;
+            this.donatRB.Text = "Donat 2$";
+            this.donatRB.UseVisualStyleBackColor = true;
+            // 
+            // cheeseRB
+            // 
+            this.cheeseRB.AutoSize = true;
+            this.cheeseRB.Location = new System.Drawing.Point(6, 88);
+            this.cheeseRB.Name = "cheeseRB";
+            this.cheeseRB.Size = new System.Drawing.Size(104, 17);
+            this.cheeseRB.TabIndex = 24;
+            this.cheeseRB.TabStop = true;
+            this.cheeseRB.Text = "Cheese Cake 4$";
+            this.cheeseRB.UseVisualStyleBackColor = true;
+            // 
+            // zefirRB
+            // 
+            this.zefirRB.AutoSize = true;
+            this.zefirRB.Location = new System.Drawing.Point(6, 111);
+            this.zefirRB.Name = "zefirRB";
+            this.zefirRB.Size = new System.Drawing.Size(61, 17);
+            this.zefirRB.TabIndex = 25;
+            this.zefirRB.TabStop = true;
+            this.zefirRB.Text = "Zefir 1$";
+            this.zefirRB.UseVisualStyleBackColor = true;
+            // 
+            // desert
+            // 
+            this.desert.Controls.Add(this.chocCakeRB);
+            this.desert.Controls.Add(this.zefirRB);
+            this.desert.Controls.Add(this.iceCream);
+            this.desert.Controls.Add(this.cheeseRB);
+            this.desert.Controls.Add(this.donatRB);
+            this.desert.Location = new System.Drawing.Point(311, 115);
+            this.desert.Name = "desert";
+            this.desert.Size = new System.Drawing.Size(128, 135);
+            this.desert.TabIndex = 26;
+            this.desert.TabStop = false;
+            // 
+            // drinks
+            // 
+            this.drinks.Controls.Add(this.cocacolaRB);
+            this.drinks.Controls.Add(this.pepsiRB);
+            this.drinks.Controls.Add(this.juiceRB);
+            this.drinks.Controls.Add(this.beerRB);
+            this.drinks.Controls.Add(this.teaRB);
+            this.drinks.Location = new System.Drawing.Point(196, 115);
+            this.drinks.Name = "drinks";
+            this.drinks.Size = new System.Drawing.Size(109, 135);
+            this.drinks.TabIndex = 27;
+            this.drinks.TabStop = false;
+            // 
+            // mainCourses
+            // 
+            this.mainCourses.Controls.Add(this.FriedEggsRB);
+            this.mainCourses.Controls.Add(this.PorridgeRB);
+            this.mainCourses.Controls.Add(this.MeatRB);
+            this.mainCourses.Controls.Add(this.PotatoRB);
+            this.mainCourses.Controls.Add(this.vegetablesRB);
+            this.mainCourses.Location = new System.Drawing.Point(29, 115);
+            this.mainCourses.Name = "mainCourses";
+            this.mainCourses.Size = new System.Drawing.Size(161, 135);
+            this.mainCourses.TabIndex = 28;
+            this.mainCourses.TabStop = false;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.calculateButton.Location = new System.Drawing.Point(29, 256);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(410, 42);
+            this.calculateButton.TabIndex = 29;
+            this.calculateButton.Text = "Order";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // showBox
+            // 
+            this.showBox.Location = new System.Drawing.Point(445, 115);
+            this.showBox.Multiline = true;
+            this.showBox.Name = "showBox";
+            this.showBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.showBox.Size = new System.Drawing.Size(219, 183);
+            this.showBox.TabIndex = 30;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label6.Location = new System.Drawing.Point(499, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 31);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "History:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 312);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.showBox);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.mainCourses);
+            this.Controls.Add(this.drinks);
+            this.Controls.Add(this.desert);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.desert.ResumeLayout(false);
+            this.desert.PerformLayout();
+            this.drinks.ResumeLayout(false);
+            this.drinks.PerformLayout();
+            this.mainCourses.ResumeLayout(false);
+            this.mainCourses.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton FriedEggsRB;
+        private System.Windows.Forms.RadioButton PorridgeRB;
+        private System.Windows.Forms.RadioButton MeatRB;
+        private System.Windows.Forms.RadioButton PotatoRB;
+        private System.Windows.Forms.RadioButton vegetablesRB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton cocacolaRB;
+        private System.Windows.Forms.RadioButton pepsiRB;
+        private System.Windows.Forms.RadioButton juiceRB;
+        private System.Windows.Forms.RadioButton teaRB;
+        private System.Windows.Forms.RadioButton beerRB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton chocCakeRB;
+        private System.Windows.Forms.RadioButton iceCream;
+        private System.Windows.Forms.RadioButton donatRB;
+        private System.Windows.Forms.RadioButton cheeseRB;
+        private System.Windows.Forms.RadioButton zefirRB;
+        private System.Windows.Forms.GroupBox desert;
+        private System.Windows.Forms.GroupBox drinks;
+        private System.Windows.Forms.GroupBox mainCourses;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox showBox;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ OrderWinForm/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace OrderWinForm
+{
+    
+    public partial class Form1 : Form
+    {
+        double sum;
+        int number;
+        DateTime dt;
+        StringBuilder str = new StringBuilder();
+        public Form1()
+        {
+            InitializeComponent();
+            sum = 0;
+            number = new Random().Next(1,100000);
+            dt = DateTime.Now;
+        }
+        private bool isDrinks()
+        {
+            if (cocacolaRB.Checked || pepsiRB.Checked || juiceRB.Checked || teaRB.Checked || beerRB.Checked)
+            {
+                if (cocacolaRB.Checked)
+                {
+                    sum += 0.50;
+                    str.Append($"\nCoca Cola  {0.50}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (pepsiRB.Checked)
+                {
+                    sum += 0.50;
+                    str.Append($"\nPepsi  {0.50}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (juiceRB.Checked)
+                {
+                    sum += 1;
+                    str.Append($"\nFruit Juice  {1}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (teaRB.Checked)
+                {
+                    sum += 1.5;
+                    str.Append($"\nTea  {1.5}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (beerRB.Checked)
+                {
+                    sum += 3;
+                    str.Append($"\nBeer  {3}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                return true;
+            }
+            return false;
+        }
+        private bool isCourses()
+        {
+            if (FriedEggsRB.Checked || PorridgeRB.Checked || MeatRB.Checked || PotatoRB.Checked || vegetablesRB.Checked)
+            {
+                if (FriedEggsRB.Checked)
+                {
+                    sum += 2;
+                    str.Append($"\nFried eggs  {2}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (PorridgeRB.Checked)
+                {
+                    sum += 3;
+                    str.Append($"\nPorridge  {3}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (MeatRB.Checked)
+                {
+                    sum += 8;
+                    str.Append($"\nMeat with sendwiches  {8}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (PotatoRB.Checked)
+                {
+                    sum += 2;
+                    str.Append($"\nFried Potato  {2}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (vegetablesRB.Checked)
+                {
+                    sum += 6;
+                    str.Append($"\nFried vegetables  {6}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                return true;
+            }
+            return false;
+        }
+        private bool isDeserts()
+        {
+
+            if (chocCakeRB.Checked || iceCream.Checked || donatRB.Checked || cheeseRB.Checked || zefirRB.Checked)
+            {
+                if (chocCakeRB.Checked)
+                {
+                    sum += 3;
+                    str.Append($"\nChocolate Cake  {3}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (iceCream.Checked)
+                {
+                    sum += 2;
+                    str.Append($"\nIce Cream  {2}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (donatRB.Checked)
+                {
+                    sum += 2;
+                    str.Append($"\nDonat  {2}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (cheeseRB.Checked)
+                {
+                    sum += 4;
+                    str.Append($"\nCheese Cake  {4}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                if (zefirRB.Checked)
+                {
+                    sum += 1;
+                    str.Append($"\nZefir {1}$,  Date: {dt},  Number: {number}\n\n");
+                }
+                return true;
+            }
+            return false;
+        }
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            sum = 0;
+            if (isDrinks()&&isDeserts()&&isCourses())
+            {
+                showBox.Text = str.ToString() ;
+                MessageBox.Show("Sum: " + sum.ToString(), "Date: " + dt + "   Number: " + number, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
